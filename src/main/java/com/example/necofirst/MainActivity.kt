394fa1a88036720d9bf0a87873a61d7e63d2d2bc
purskyi <purskyipro@gmail.com>
2024-09-17 22:39:09 +0300

    click

--- src/main/java/com/example/necofirst/MainActivity.kt
+++ src/main/java/com/example/necofirst/MainActivity.kt
@@ -1,25 +1,46 @@
 package com.example.necofirst
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.necofirst.ui.theme.NecoFirstTheme
@@ -30,38 +51,65 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-Row(modifier = Modifier
-    .background(Color.Gray)
-    .fillMaxSize(),
-    horizontalArrangement = Arrangement.SpaceBetween,
-    verticalAlignment = Alignment.CenterVertically) {
+            var name = "Andrii"
+            var job = "Android Developer"
 
 
-    Column(
-        modifier = Modifier
-            .weight(1f)
-            .fillMaxHeight(0.5f)
-            .background(Color.Red),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "mine")
-        Text(text = "yours")
-        Text(text = "ours")
-    }
-
-            Column(
-                modifier = Modifier
-                    .background(Color.Yellow),
-           ) {
-                Text(text = "mine")
-                Text(text = "yours")
-                Text(text = "ours")
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())) {
+                myApp(name,job)
+                myApp(name,job)
+                myApp(name,job)
+                myApp(name,job)
+                myApp(name,job)
+                myApp(name,job)
+                myApp(name,job)
+                myApp(name,job)
             }
 
-}
-}
 
+        }
     }
 }
 
+
+
+
+@Composable
+fun myApp(name: String, job: String, modifier: Modifier = Modifier) {
+
+    Card(modifier = modifier
+        .pointerInput(Unit){
+            detectDragGestures{ change, dragAmount -> Log.d("My log", "  $dragAmount")  }}
+        .fillMaxWidth()
+        .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+
+    ){ Box(modifier = Modifier
+        .clickable { Log.d("My log", "Clked") }
+        .fillMaxWidth()
+        .background(Color.Cyan)){
+        Row(modifier = Modifier
+            .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Image(painter = painterResource(
+                id = R.drawable.android_superhero1),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(CircleShape))
+
+            Column(modifier = Modifier
+                .padding(16.dp)){
+                Text(text = name,)
+                Text(text = job)
+            }
+
+        }
+    }
+    }
+
+}
