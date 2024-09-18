c44e13ae76f2763c3fc767003bd7580a85c143af
purskyi <purskyipro@gmail.com>
2024-09-18 20:15:29 +0300

    my own total counter and Scafold and ai_rememberCountersOfCardsAfterScrollingBehindScreen

--- src/main/java/com/example/necofirst/MainActivity.kt
+++ src/main/java/com/example/necofirst/MainActivity.kt
@@ -14,13 +14,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -28,11 +31,18 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,6 +51,8 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.necofirst.ui.theme.NecoFirstTheme
@@ -51,44 +63,73 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
+
+
             var name = "Andrii"
             var job = "Android Developer"
 
 
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())) {
-                myApp(name,job)
-                myApp(name,job)
-                myApp(name,job)
-                myApp(name,job)
-                myApp(name,job)
-                myApp(name,job)
-                myApp(name,job)
-                myApp(name,job)
-            }
-
+          myWholeApp(name = name, job = job )
 
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarWithAppName(total: Int = 0, modifier: Modifier = Modifier) {
+
+    CenterAlignedTopAppBar(
+        title = {
+           Row(modifier = modifier) {
+               Text(text = total.toString())
+
+           }
+        })
 
 
+}
 
 @Composable
-fun myApp(name: String, job: String, modifier: Modifier = Modifier) {
+fun myWholeApp(name: String, job: String, modifier: Modifier = Modifier){
+    var total by remember { mutableStateOf(0) }
+    val counters = remember { mutableStateListOf(*Array(10) { 0 }) }
+
+    Scaffold(topBar = { TopAppBarWithAppName(total) })
+    { BarPadding ->
+    LazyColumn(contentPadding = BarPadding) {
+        items(10) { index ->
+            myApp(
+                name = name,
+                job = job,
+                counterValue = counters[index],
+                onCounterClick = { total++
+                counters[index]++}
+            )
+
+        }
+        }
+    }
+}
+
+@Composable
+fun myApp(name: String, job: String, counterValue: Int, onCounterClick: () -> Unit, modifier: Modifier = Modifier) {
+    var counter by remember { mutableStateOf(0) }
 
     Card(modifier = modifier
-        .pointerInput(Unit){
-            detectDragGestures{ change, dragAmount -> Log.d("My log", "  $dragAmount")  }}
+        /*  .pointerInput(Unit) {
+            detectDragGestures { change, dragAmount -> Log.d("My log", "  $dragAmount") }
+        }*/
         .fillMaxWidth()
-        .padding(10.dp),
+        .padding(10.dp)
+        .clickable {
+            onCounterClick()
+        },
         shape = RoundedCornerShape(15.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
 
     ){ Box(modifier = Modifier
-        .clickable { Log.d("My log", "Clked") }
+        //   .clickable { Log.d("My log", "Clked") }
         .fillMaxWidth()
         .background(Color.Cyan)){
         Row(modifier = Modifier
@@ -106,6 +147,7 @@ fun myApp(name: String, job: String, modifier: Modifier = Modifier) {
                 .padding(16.dp)){
                 Text(text = name,)
                 Text(text = job)
+                Text(text = counterValue.toString())
             }
 
         }
